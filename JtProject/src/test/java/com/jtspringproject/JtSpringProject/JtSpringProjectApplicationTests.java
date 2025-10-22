3ee7b86df2429a807573354bc8fc8f0330e094e9
patilyash7072 <126942065+patilyash7072@users.noreply.github.com>
2025-10-22 16:43:10 +0530

    fdfdeff

--- JtProject/src/test/java/com/jtspringproject/JtSpringProject/JtSpringProjectApplicationTests.java
+++ JtProject/src/test/java/com/jtspringproject/JtSpringProject/JtSpringProjectApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class JtSpringProjectApplicationTests {
 
-	@Test
+	@Testfdgdfgfdgdfgdfgdf
 	void contextLoads() {
 	}
 
